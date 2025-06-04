3038c82306f409e38f66db0345edb1ba5da666bf
Frijidia <espoir.agoli-agbo@epitech.eu>
2025-06-04 17:00:30 +0100

    adding model to controllers to create tables

--- backend/src/modules/fichiers/fichierControllers.js
+++ backend/src/modules/fichiers/fichierControllers.js
@@ -1,9 +1,9 @@
+import "./fichierModels.js";
 import pool from '../../config/database.js';
 import s3 from '../../config/aws.js';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import mime from "mime-types";
-import "./fichierModels.js";
 // import fs from 'fs/promises';
 
 
